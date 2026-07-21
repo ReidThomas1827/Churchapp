f27b22227dc37f95922c429922f78d52b673c7ee
Reid Thomas <reidcthomas.05@gmail.com>
2026-07-20 21:18:55 -0400

    Temp diagnostic: list env var names to debug Notion key

--- sermonapp/functions/api/health.js
+++ sermonapp/functions/api/health.js
@@ -11,4 +11,6 @@ export const onRequestGet = ({ env }) =>
     push: !!(env.VAPID_PUBLIC_KEY && env.VAPID_PRIVATE_KEY),
     vapidPublicKey: env.VAPID_PUBLIC_KEY || "",
     notion: !!env.NOTION_API_KEY,
+    diag: 1,
+    envKeys: Object.keys(env),
   });
