6505f4b90b56d0016d5a787d76c2ce99fbf564d6
Reid Thomas <reidcthomas.05@gmail.com>
2026-07-21 21:06:35 -0400

    Remove temporary env-var diagnostic from health endpoint

--- sermonapp/functions/api/health.js
+++ sermonapp/functions/api/health.js
@@ -11,6 +11,4 @@ export const onRequestGet = ({ env }) =>
     push: !!(env.VAPID_PUBLIC_KEY && env.VAPID_PRIVATE_KEY),
     vapidPublicKey: env.VAPID_PUBLIC_KEY || "",
     notion: !!env.NOTION_API_KEY,
-    diag: 1,
-    envKeys: Object.keys(env),
   });
